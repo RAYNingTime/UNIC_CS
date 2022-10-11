587f439d9afd51536992cdf12ad52f7b87ee5681
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-10-11 18:14:01 +0300

    Create ExamClass.cs

---
+++ VP/09-09-2022/HW1/v2/ExamClass.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ExamClass;
+
+public class Exam
+{
+    public string Question { get; set; }
+    public string CorrectAnswer { get; set; }
+    public string[] AnswerPool { get; set; }
+
+    public Exam(string question, string answer, string[] incorrectAnswers)
+    {
+        Random random = new Random();
+        int i;
+
+        Question = question;
+
+        CorrectAnswer = answer;
+
+        // Collapsing two arrays to one
+        string[] tempArray = incorrectAnswers;
+        string[] newArr = new string[incorrectAnswers.Length + 1];
+        for (i = 0; i < tempArray.Length; i++)
+            newArr[i] = tempArray[i];
+        newArr[i] = answer;
+
+        AnswerPool = newArr;
+
+        // Shuffle of the array
+        AnswerPool = AnswerPool.OrderBy(x => random.Next()).ToArray();
+    }
+}
