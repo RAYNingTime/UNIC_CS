360858525789f2b8841b259a7f22910432c16fa2
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2023-01-27 10:44:04 +0200

    Add files via upload

---
+++ VP/Homework_5/HW5Ex0/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.IO;
+using System.Windows.Media.TextFormatting;
+
+namespace HW5Ex0
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddStudentButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Error Handling
+            if (string.IsNullOrEmpty(FirstNameTextBox.Text))
+            {
+                MessageBox.Show("Please enter first name", "Invalid first name", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else if (string.IsNullOrEmpty(LastNameTextBox.Text))
+            {
+                MessageBox.Show("Please enter last name", "Invalid last name", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else if (string.IsNullOrEmpty(AdressTextBox.Text))
+            {
+                MessageBox.Show("Please enter adress", "Invalid adress", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else if (string.IsNullOrEmpty(PostCodeTextBox.Text))
+            {
+                MessageBox.Show("Please enter post code", "Invalid post code", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else if (!int.TryParse(PostCodeTextBox.Text, out int postCode))
+            {
+                MessageBox.Show("Please enter valid post code", "Invalid post code", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else if (postCode < 0)
+            {
+                MessageBox.Show("Please enter positive post code", "Invalid post code", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Creating new Student CheckBox
+            CheckBox newCheckBox = new CheckBox();
+            newCheckBox.Content = FirstNameTextBox.Text + " " + LastNameTextBox.Text + " (" + ClassStandingComboBox.Text + ")";
+            StudentStackPanel.Children.Add(newCheckBox);
+        }
+
+        private void ImportStudentsButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Importing new students line by line
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                String fileName = openFileDialog.FileName;
+                String[] importedStudents = File.ReadAllLines(fileName);
+
+                foreach(var student in importedStudents)
+                {
+                    CheckBox newCheckBox = new CheckBox();
+                    newCheckBox.Content = student;
+                    newCheckBox.Tag = student;
+                    StudentStackPanel.Children.Add(newCheckBox);
+                }
+            }
+        }
+
+        private void ExportStudentsButton_Click(object sender, RoutedEventArgs e)
+        {
+            String saveData = "";
+            bool selected = false;
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if(saveFileDialog.ShowDialog() == true)
+            {
+                String fileName = saveFileDialog.FileName;
+
+                for (int i = 0; i < StudentStackPanel.Children.Count; i++)
+                {
+                    CheckBox studentCheckBox = StudentStackPanel.Children[i] as CheckBox;
+
+                    if (studentCheckBox.IsChecked == true)
+                    {
+                        saveData += studentCheckBox.Content + "\n";
+                        StudentStackPanel.Children.RemoveAt(i);
+                        i--;
+
+                        selected = true;
+                    }
+
+                }
+
+                // Error Handling if no students selected
+                if (!selected)
+                    MessageBox.Show("To export data you should select minimum one student!", "Student is not selected", MessageBoxButton.OK, MessageBoxImage.Error);
+                else
+                    File.WriteAllText(fileName, saveData);
+            }
+        }
+    }
+}
---
+++ VP/Homework_5/HW5Ex1/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW5Ex1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Error Handling
+            if (string.IsNullOrEmpty(TitleTextBox.Text))
+            {
+                MessageBox.Show("Please enter title", "Invalid title", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else if (string.IsNullOrEmpty(DescriptionTextBox.Text))
+            {
+                MessageBox.Show("Please enter description", "Invalid description", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Creating Remove Button
+            Button button = new Button();
+            button.Content = "Remove";
+            button.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#8B0000");
+            button.Foreground = new SolidColorBrush(Colors.White);
+            button.VerticalAlignment = VerticalAlignment.Center;
+            button.HorizontalAlignment = HorizontalAlignment.Left;
+            button.Tag = button;
+            button.Click += Button_Click;
+
+            // Creating TextBlock that is going to contain Description
+            // TextBlock instead of label because I want use TextWrapping
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = DescriptionTextBox.Text;
+            textBlock.TextWrapping = TextWrapping.Wrap;
+
+            // Creating Stack Panel to put inside of the expander
+            StackPanel stackPanel = new StackPanel();
+            stackPanel.Children.Add(textBlock);
+            stackPanel.Children.Add(button);
+
+            // Creating Expander that contains Title as a header and Stack Panel as a Content
+            Expander expander = new Expander();
+            expander.Header = TitleTextBox.Text;
+            expander.Content = stackPanel;
+
+            BoardStackPanel.Children.Add(expander);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+
+            if (button != null)
+            {
+                // For Each expander in Stack panel
+                foreach(Expander expander in BoardStackPanel.Children)
+                {
+                    // Selecting Content of the Expander
+                    StackPanel stackPanel = expander.Content as StackPanel;
+
+                    // And checking if Children at position 1 (Always button) is the same as our sender of the event
+                    if (stackPanel.Children[1] == button)
+                    {
+                        BoardStackPanel.Children.Remove(expander);
+                        // If found leaving the event, otherwise it'll crash...
+                        return;
+                    }
+                }
+            }
+
+        }
+    }
+}
